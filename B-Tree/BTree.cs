be8637c735ea0f10861a8769176f481e2a10dfef
Maksim <maksalmak@gmail.com>
2018-03-13 15:05:15 +0300

    Added mock for second task

---
+++ B-Tree/BTree.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace B_Tree
+{
+    class BTree<T> where T : IInTreeValue
+    {
+        private Node<T> head = new Node<T>();
+        public void Add(IInTreeValue value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get(int key)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ B-Tree/Node.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace B_Tree
+{
+    internal class Node<T> where T : IInTreeValue
+    {
+        private List<T> values = new List<T>();
+        private List<Node<T>> childs = new List<Node<T>>();
+    }
+}
